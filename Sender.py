f2d204f73b1075eb1c3b52c99ce587363d4e06a8
Evan White <emwhite3@miners.utep.edu>
2020-10-09 11:16:13 -0600

    CLeaned comments and code Added inline file and protocol input

--- Sender.py
+++ Sender.py
@@ -1,17 +1,16 @@
 import socket
 import _thread
+import sys
 import time
-import string
 import packet
 import udt
-import random
 from timer import Timer
 
 # Some already defined parameters
 PACKET_SIZE = 512
 RECEIVER_ADDR = ('localhost', 8080)
 SENDER_ADDR = ('localhost', 9090)
-SLEEP_INTERVAL = 0.05 # (In seconds)
+SLEEP_INTERVAL = 0.05               # (In seconds)
 TIMEOUT_INTERVAL = 0.5
 WINDOW_SIZE = 4
 
@@ -23,16 +22,7 @@ timer = Timer(TIMEOUT_INTERVAL)
 # Need to have two threads: one for sending and another for receiving ACKs
 
 
-# Generate random payload of any length
-def generate_payload(length=10):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-
-    return result_str
-
-
-def read_payload():
-    file = "bio.txt"
+def read_payload(file):
     try:
         file = open(file, 'rb')
     except IOError:
@@ -51,55 +41,21 @@ def read_payload():
 
 # Send using Stop_n_wait protocol
 def send_snw(sock):
-    # Fill out the code here
-    seq = 0
-    clock = Timer(TIMEOUT_INTERVAL)
-    while seq < 20:
-        # read packet from bio
-        data = read_payload()
-        # create packet from data
-        pkt = packet.make(seq, data)
-        # send packet
-        udt.send(pkt, sock, RECEIVER_ADDR)
-        seq += 512
-        # start timer
-        clock.start()
-        # wait to receive packet
-        while not clock.timeout():
-            pktR, addrR = udt.recv()
-            # check_corrupt(pktR), check_seq_num() continue
-            # if not corrupt, is ack and has proper sequence number break
-            # if timeout resend pkt and restart timer
-        # if we receive packet and is not corrupt or is ack with improper sequence number stay in loop
-        # if we timeout resend packet and start timer
-        # if we receive a packet and it is not corrupt and it is an ack with proper sequence number we stop timer and continue
-        while not clock.timeout():
-            pktR, addrR = udt.recv()
-        continue
-        # wait to see if we receive another packet of data, if not repeat these steps starting from read packet
+    print("starting snw thread")
 
-
-    #     data = generate_payload(40).encode()
-    #     pkt = packet.make(seq, data)
-    #     print("Sending seq# ", seq, "\n")
-    #     udt.send(pkt, sock, RECEIVER_ADDR)
-    #     seq = seq+1
-    #     time.sleep(TIMEOUT_INTERVAL)
-    # pkt = packet.make(seq, "END".encode())
-    # udt.send(pkt, sock, RECEIVER_ADDR)
 
 def get_window_size(num_packets):
     global base
     return min(WINDOW_SIZE, num_packets - base)
 
+
 # Send using GBN protocol
-def send_gbn(sock):
+def send_gbn(sock, packets):
     global mutex
     global base
     global timer
 
     # get packets from file and set all necessary values
-    packets = read_payload()
     num_packets = len(packets)
     print("Packets to be sent: %s" % num_packets)
     window_size = get_window_size(num_packets)
@@ -128,10 +84,10 @@ def send_gbn(sock):
         # Wait until a timer times out and pass mutex lock to receive thread
         while timer.running() and not timer.timeout():
             mutex.release()
-            print("Waiting for acks")
+            print("Waiting for ACKs")
             time.sleep(SLEEP_INTERVAL)
             mutex.acquire()
-        # if we time out or we have not received acks for all window packets
+        # if we time out or we have not received ACKs for all window packets
         # then we reset window. Else we move the window
         if timer.timeout() or ((window_start + window_size) > base):
             print("Window timed out")
@@ -151,8 +107,9 @@ def send_gbn(sock):
 
 # Receive thread for stop-n-wait
 def receive_snw(sock, pkt):
-    # Fill here to handle acks
+    # Fill here to handle ACKs
     return
+
 
 # Receive thread for GBN
 def receive_gbn(sock):
@@ -166,28 +123,33 @@ def receive_gbn(sock):
         ack, data = packet.extract(pkt)
 
         print("Received ack: %s" % ack)
-        if ack >= base:                                 # check if ack is expected ack seq #
-            mutex.acquire()                             # lock this thread
-            base = ack + 1                              # increment base seq
-            print("Updated next sequence #: %s" % base) # update next base seq #
+        if ack >= base:                                  # check if ack is expected ack seq #
+            mutex.acquire()                              # lock this thread
+            base = ack + 1                               # increment base seq
+            print("Updated next sequence #: %s" % base)  # update next base seq #
             timer.stop()
             mutex.release()
 
 
 # Main function
 if __name__ == '__main__':
-    # if len(sys.argv) != 2:
-    #     print('Expected filename as command line argument')
-    #     exit()
+    if len(sys.argv) != 3:
+        print("Expected filename and send protocol as command line argument")
+        exit()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(SENDER_ADDR)
 
-    # filename = sys.argv[1]
-    print("starting sender gbn")
-    print("Starting gbn send thread")
-    _thread.start_new_thread(send_gbn, (sock, ))
-    exit_program = input("Enter \"quit\" to exit program")
+    filename = sys.argv[1]
+    protocol = sys.argv[2]
+    if protocol == "gbn":
+        print("starting sender gbn")
+        print("Starting gbn send thread")
+        _thread.start_new_thread(send_gbn, (sock, read_payload(filename), ))
+        exit_program = input("Enter \"quit\" to exit program")
+    elif protocol == "snw":
+        print("starting sender snw")
+        print("Starting snw send thread")
+        _thread.start_new_thread(send_snw, (sock,))
+        exit_program = input("Enter \"quit\" to exit program")
     sock.close()
-
-
