a84096bdfe09ab7189af239113a10c29a32fee3f
tlwilliams2 <35967404+tlwilliams2@users.noreply.github.com>
2020-10-09 22:39:44 -0600

    Update Receiver.py
    Added in receiver_snw implementation

--- Receiver.py
+++ Receiver.py
@@ -46,13 +46,54 @@ def receive_sr(sock, windowsize):
 
 
 # Receive packets from the sender w/ Stop-n-wait protocol
+
+ack = 0
+
 def receive_snw(sock):
-   endStr = ''
-   while endStr != 'END':
-       pkt, senderaddr = udt.recv(sock)
-       seq, data = packet.extract(pkt)
-       endStr = data.decode()
-       print("From: ", senderaddr, ", Seq# ", seq, endStr)
+    global ack
+    endStr = ''
+    prevData = '' #Holds previous data incase of packet drop from Receiver to Sender
+    #If data from packet contains 'END', receive stream has ended.
+    while endStr!='END':
+        #Extracts packet and sender address from socket.
+        pkt, senderaddr = udt.recv(sock)
+        #Extracts sequence number and data from packet.
+        seq, data = packet.extract(pkt)
+        #If data equals the previous data sent then packet was dropped from Receiver
+        #to sender. Resend previous ack number with data.
+        if data == prevData:
+            print("Data received is previous data received. Resending previous ack.")
+            if(ack == 0):
+                pkt = packet.make(ack+1, data)
+                udt.send(pkt,sock,senderaddr)
+            else:
+                pkt = packet.make(ack-1, data)
+                udt.send(pkt, sock, senderaddr)
+        
+        #If sequence does not equal ack from Sender then packet dropped causing 
+        #misorder. Send the correct sequence from Receiver back to Sender.
+        elif (seq!=ack):
+            print("Seq ", seq, " and ack ", ack, " do not match. Waiting for resend.")
+            pkt = packet.make(ack, data)
+            udt.send(pkt,sock,senderaddr)
+        
+        #Sequence matches ack from Sender.
+        #Write data to new file, incrememnt Receiver ack, send back the Sender ack
+        #to acknowledge receieved proper sequenced number.
+        else:
+            print("Seq ", seq, " and ack ", ack, " match. Will write to file.")
+            endStr = data.decode()
+            file = open("receiver_bio.txt","a")
+            file.write(endStr)
+            pkt = packet.make(ack, data)
+            prevData = data
+            
+            if(ack == 0):
+                ack = 1
+            else:
+                ack = 0
+            udt.send(pkt,sock,senderaddr)
+#       print("From: ", senderaddr, ", Seq# ", seq, endStr)
 
 
 # Main function
